19b03b2ddd8920c94df5c7f541fb4cedb6dabdf8
Hemant Singh Parmar <hemantsinghparmar@mgmtech.org>
2024-07-07 09:33:54 +0530

    added wallet

--- src/components/casino/GridGame.jsx
+++ src/components/casino/GridGame.jsx
@@ -66,45 +66,56 @@ const GridGame = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex justify-center items-center">
-      <div className="mr-10 border p-7 ">
-        <p className="mb-2">Enter a bet amount</p>
-        <input
-          type="number"
-          value={betAmount}
-          min={0}
-          onChange={(e) => setBetAmount(e.target.value)}
-          placeholder="0.000"
-          className="px-4 py-2 mb-2 w-full text-black"
-        />
-
-        <button
-          onClick={handleBet}
-          className={`px-4 py-2 w-full ${
-            !betAmount ? `bg-green-700` : `bg-green-500`
-          }  rounded hover:bg-green-700 transition`}
-          disabled={!betAmount}
-        >
-          Bet
-        </button>
+    <div className="min-h-screen bg-gray-900 text-white">
+      <div className="flex flex-col items-center justify-center p-20">
+        <span className="border rounded-md">
+          <span className="px-2 py-3"> {"0.0000 ₹"} </span>
+          <button className="bg-blue-500 font-semibold px-2 py-3 rounded-e-md hover:bg-blue-600">
+            {" "}
+            Wallet{" "}
+          </button>
+        </span>
       </div>
-      <div className="flex items-center justify-center">
-        <div className="grid grid-cols-3 gap-4 w-full max-w-xs mx-auto p-5 ">
-          {selectedNumbers.map((number, index) => (
-            <div
-              key={index}
-              onClick={() => {
-                handleInputClick(index);
-              }}
-              className="hover:transition-transform hover:scale-105 bg-gray-800 hover:bg-gray-700 relative flex items-center justify-center w-20 h-20 border-2 border-gray-700 rounded cursor-pointer"
-            >
-              <div className="absolute inset-0 flex items-center justify-center">
-                {number}
-              </div>
-            </div>
-          ))}
+      <div className="flex justify-center items-center">
+        <div className="mr-10 border p-7 ">
+          <p className="mb-2">Enter a bet amount</p>
+          <input
+            type="number"
+            value={betAmount}
+            min={0}
+            onChange={(e) => setBetAmount(e.target.value)}
+            placeholder="0.000"
+            className="px-4 py-2 mb-2 w-full text-black"
+          />
+
+          <button
+            onClick={handleBet}
+            className={`px-4 py-2 w-full ${
+              !betAmount ? `bg-green-700` : `bg-green-500`
+            }  rounded hover:bg-green-700 transition`}
+            disabled={!betAmount}
+          >
+            Bet
+          </button>
         </div>
-        {result && <div className="m-4 text-xl ">{result}</div>}
+        <div className="flex items-center justify-center">
+          <div className="grid grid-cols-3 gap-4 w-full max-w-xs mx-auto p-5 ">
+            {selectedNumbers.map((number, index) => (
+              <div
+                key={index}
+                onClick={() => {
+                  handleInputClick(index);
+                }}
+                className="hover:transition-transform hover:scale-105 bg-gray-800 hover:bg-gray-700 relative flex items-center justify-center w-20 h-20 border-2 border-gray-700 rounded cursor-pointer"
+              >
+                <div className="absolute inset-0 flex items-center justify-center">
+                  {number}
+                </div>
+              </div>
+            ))}
+          </div>
+          {result && <div className="m-4 text-xl ">{result}</div>}
+        </div>
       </div>
     </div>
   );
